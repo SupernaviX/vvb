94a4d4170ce0e077c7e358265528f5eda81bac7b
Simon Gellis <simongellis@gmail.com>
2020-07-31 01:06:13 -0400

    No more crashing, but no rendering

--- src/lib.rs
+++ src/lib.rs
@@ -5,42 +5,47 @@ mod renderer;
 use jni::JNIEnv;
 use jni::objects::JObject;
 use jni::sys::{jint};
+use jni::errors::Error as JNIError;
 use std::sync::MutexGuard;
 use renderer::Renderer;
 
 const STATE_FIELD: &str = "_rendererPtr";
 
-macro_rules! safe_unwrap {
-    ($action:expr, $env:expr) => {
-        match $action {
-            Ok(res) => Some(res),
-            Err(err) => {
-                $env.throw(err.to_string());
-                None
+trait ResultExt<T, E> {
+    fn to_java_exception(&self, env: &JNIEnv);
+}
+impl<T, E> ResultExt<T, E> for Result<T, E> where E: ToString {
+    fn to_java_exception(&self, env: &JNIEnv) {
+        match self {
+            Ok(_) => (),
+            Err(error) => {
+                env.throw(error.to_string()).expect("Throwing an error itself caused an error");
             }
         }
     }
 }
 
-fn read_state<F: FnOnce(MutexGuard<Renderer>) -> ()>(env: &JNIEnv, this: &JObject, action: F) -> () {
-    match env.get_rust_field(*this, STATE_FIELD) {
-        Ok(res) => action(res),
-        Err(err) => {
-            env.throw(err.to_string()).expect("Getting a rust field threw!");
-        }
-    }
+fn read_state<F: FnOnce(MutexGuard<Renderer>) -> Result<(), String>>(env: &JNIEnv, this: &JObject, action: F) -> () {
+    env.get_rust_field(*this, STATE_FIELD)
+        .map_err(|err| { err.to_string() })
+        .and_then(|state: MutexGuard<Renderer>| { action(state) })
+        .to_java_exception(&env);
 }
 
 #[no_mangle]
 pub unsafe extern fn Java_com_simongellis_vvb_MainActivity_nativeOnDestroy(env: JNIEnv, this: JObject) -> () {
-    let _: Option<Renderer> = safe_unwrap!(env.take_rust_field(this, STATE_FIELD), &env);
+    let res: Result<Renderer, JNIError> = env.take_rust_field(this, STATE_FIELD);
+    res.to_java_exception(&env);
 }
 
 #[no_mangle]
 pub unsafe extern fn Java_com_simongellis_vvb_MainActivity_nativeOnSurfaceCreated(env: JNIEnv, this: JObject) -> () {
-    safe_unwrap!(Renderer::new(), env).and_then(|state| {
-        safe_unwrap!(env.set_rust_field(this, STATE_FIELD, state), env)
-    });
+    Renderer::new()
+        .and_then(|state| {
+            env.set_rust_field(this, STATE_FIELD, state)
+                .map_err(|err| { err.to_string() })
+        })
+        .to_java_exception(&env);
 }
 
 
@@ -48,12 +53,13 @@ pub unsafe extern fn Java_com_simongellis_vvb_MainActivity_nativeOnSurfaceCreate
 pub unsafe extern fn Java_com_simongellis_vvb_MainActivity_nativeOnSurfaceChanged(env: JNIEnv, this: JObject, width: jint, height: jint) -> () {
     read_state(&env, &this, |mut state| {
         state.on_surface_changed(width, height);
+        Ok(())
     });
 }
 
 #[no_mangle]
 pub unsafe extern fn Java_com_simongellis_vvb_MainActivity_nativeOnDrawFrame(env: JNIEnv, this: JObject) -> () {
     read_state(&env, &this, |state| {
-        state.on_draw_frame();
+        state.on_draw_frame()
     });
 }
--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -13,25 +13,15 @@ mod gl {
 }
 
 use gl::types::{GLboolean,GLint,GLuint,GLchar,GLsizei,GLsizeiptr,GLvoid,GLfloat,GLenum};
-use std::ffi::{CString};
+use std::ffi::{CStr,CString};
+use std::ptr::{null_mut, null};
 
-const GL_TRUE: GLboolean = 0;
+const GL_TRUE: GLboolean = 1;
 const GL_FALSE: GLboolean = 0;
 
 macro_rules! c_string {
     ($string:expr) => {
         CString::new($string).map_err(|_| { "Could not build c string!" })
-    };
-}
-
-macro_rules! gl_gen {
-    ($func:ident, $type:ty, $action:literal) => {
-        {
-            let mut temp_array: [$type;1] = [0];
-            gl::$func(1, temp_array.as_mut_ptr());
-            check_error($action)?;
-            temp_array[0]
-        }
     };
 }
 
@@ -41,26 +31,45 @@ macro_rules! gl_voidptr {
     };
 }
 
+fn gl_temp_array<T: Copy + Default, F: FnOnce(*mut T) -> ()>(cb: F) -> T {
+    let mut tmp_array: [T;1] = Default::default();
+    cb(tmp_array.as_mut_ptr());
+    tmp_array[0]
+}
+
 const VERTEX_SHADER: &str = "
-layout (location = 0) attribute vec2 a_Pos
+attribute vec2 a_Pos;
 uniform mat4 u_Model;
-varying vec2 v_TexCoords;
+varying mediump vec2 v_TexCoords;
 
 void main() {
     gl_Position = u_Model * vec4(a_Pos, 0.0, 1.0);
-    v_TexCoords = i_Pos;
+    v_TexCoords = a_Pos;
 }
 ";
 
 const FRAGMENT_SHADER: &str = "\
-varying vec2 v_TexCoords;
+varying mediump vec2 v_TexCoords;
 uniform sampler2D u_Texture;
-out vec4 o_Color;
 
 void main() {
-    o_Color = texture(u_Texture, v_TexCoords);
+    gl_FragColor = texture2D(u_Texture, v_TexCoords);
 }
 ";
+
+const SQUARE_VERTICES: [GLfloat; 8] = [
+    -1.0, -1.0,
+    1.0, -1.0,
+    -1.0, 1.0,
+    1.0, 1.0
+];
+
+const TEXTURE_VERTICES: [GLfloat; 8] = [
+    1.0, 1.0,
+    1.0, 0.0,
+    0.0, 1.0,
+    0.0, 0.0
+];
 
 const QUAD_VERTICES: [f32; 12] = [
     0.0, 1.0,
@@ -76,31 +85,60 @@ const VB_HEIGHT: i32 = 224;
 const TEXTURE_SIZE: usize = (VB_WIDTH * VB_HEIGHT * 2 * 3) as usize;
 
 pub struct Renderer {
-    texture_id: u32,
-    texture_data: Vec<f32>,
-    vertex_buffer_id: u32,
-    position_location: u32,
-    model_location: i32,
+    texture_id: GLuint,
+    texture_data: Vec<GLfloat>,
+    vertex_buffer_id: GLuint,
+    position_location: GLuint,
+    model_location: GLint,
+    texture_location: GLint,
     scale: f32,
     offset: (f32, f32)
 }
 
 unsafe fn make_shader(type_: GLenum, source: &str) -> Result<GLuint, String> {
     let shader_id = gl::CreateShader(type_);
-    let shader_source = [c_string!(source)?.as_ptr()].as_ptr();
-    let shader_length = [source.len() as GLint].as_ptr();
-    gl::ShaderSource(shader_id, 1, shader_source, shader_length);
+    let shader_str = c_string!(source)?;
+    let shader_source = [shader_str.as_ptr()].as_ptr();
+    gl::ShaderSource(shader_id, 1, shader_source, null());
     check_error("load a shader's source")?;
     gl::CompileShader(shader_id);
     check_error("compile a shader")?;
+    check_shader(type_, shader_id)?;
     Ok(shader_id)
+}
+
+unsafe fn check_shader(type_: GLenum, shader_id: GLuint) -> Result<(), String> {
+    let status = gl_temp_array(|ptr| {
+        gl::GetShaderiv(shader_id, gl::COMPILE_STATUS, ptr)
+    }) as GLboolean;
+    check_error("checking compile status of a shader")?;
+    if status == GL_TRUE {
+        return Ok(());
+    }
+
+    let length = gl_temp_array(|ptr| {
+        gl::GetShaderiv(shader_id, gl::INFO_LOG_LENGTH, ptr);
+    });
+    check_error("finding info log length for a shader")?;
+    if length < 0 {
+        return Err("Invalid shader info log length")?;
+    }
+    let mut buf = vec!(0; length as usize);
+    let buf_ptr = buf.as_mut_ptr() as *mut GLchar;
+    gl::GetShaderInfoLog(shader_id, length, null_mut(), buf_ptr);
+    let cstr = CStr::from_bytes_with_nul(buf.as_slice())
+        .map_err(|err| { err.to_string() })?;
+
+    let log = cstr.to_str()
+        .map_err(|err| { err.to_string() })?;
+    Err(format!("Error compiling shader type {}! <{}>", type_, log))
 }
 
 fn check_error(action: &str) -> Result<(), String> {
     let error = unsafe { gl::GetError() };
     match error {
         gl::NO_ERROR => Ok(()),
-        _ => Err(format!("OpenGL threw code {} while trying to {}!", error, action))
+        _ => Err(format!("OpenGL threw code 0x{:04X} while trying to {}!", error, action))
     }
 }
 
@@ -126,25 +164,28 @@ impl Renderer {
 
             let position_location = gl::GetAttribLocation(program, c_string!("a_Pos")?.as_ptr()) as u32;
             let model_location = gl::GetUniformLocation(program, c_string!("u_Model")?.as_ptr());
+            let texture_location = gl::GetUniformLocation(program, c_string!("u_Texture")?.as_ptr());
             check_error("get uniform location")?;
 
-            let texture_id = gl_gen!(GenTextures, GLuint, "generate a texture");
+            let texture_id = gl_temp_array(|ptr| { gl::GenTextures(1, ptr) });
+            check_error("generate a texture")?;
             let mut texture_data = vec![0.0; TEXTURE_SIZE];
             // TODO: load texture
             for r in (0..TEXTURE_SIZE).step_by(3) {
                 let i = r / 3;
                 let checker_x = (i / 16) % 2;
-                let checker_y = 1 - ((i / 224) / 16);
+                let checker_y = 1 - ((i / 224) / 16) % 2;
                 if checker_x != checker_y {
                     texture_data[r] = 1.0;
                 }
             }
 
+            gl::ActiveTexture(gl::TEXTURE0);
             gl::BindTexture(gl::TEXTURE_2D, texture_id);
-            gl::TexImage2D(gl::TEXTURE_2D, 0, gl::RGB as i32, VB_WIDTH, VB_HEIGHT, 0, gl::RGBA, gl::UNSIGNED_BYTE, gl_voidptr!(texture_data));
+            gl::TexImage2D(gl::TEXTURE_2D, 0, gl::RGB as i32, VB_WIDTH, VB_HEIGHT, 0, gl::RGB, gl::UNSIGNED_BYTE, gl_voidptr!(texture_data));
             check_error("load a texture")?;
 
-            let vertex_buffer_id = gl_gen!(GenBuffers, GLuint, "generate a buffer");
+            let vertex_buffer_id = gl_temp_array(|ptr| { gl::GenBuffers(1, ptr) });
             gl::BindBuffer(gl::ARRAY_BUFFER, vertex_buffer_id);
             gl::BufferData(gl::ARRAY_BUFFER, QUAD_VERTICES.len() as GLsizeiptr, gl_voidptr!(QUAD_VERTICES), gl::STATIC_DRAW);
             check_error("load a buffer")?;
@@ -158,6 +199,7 @@ impl Renderer {
                 vertex_buffer_id,
                 position_location,
                 model_location,
+                texture_location,
                 scale: VB_WIDTH as f32,
                 offset: (0.0, 0.0),    
             }
@@ -185,7 +227,7 @@ impl Renderer {
         }
     }
 
-    pub fn on_draw_frame(&self) {
+    pub fn on_draw_frame(&self) -> Result<(), String> {
         let model: [GLfloat; 16] = [
             self.scale, 0.0, 0.0, self.offset.0,
             0.0, self.scale, 0.0, self.offset.1,
@@ -194,14 +236,21 @@ impl Renderer {
         ];
         unsafe {
             gl::Clear(gl::COLOR_BUFFER_BIT);
+            check_error("clear the screen")?;
 
             gl::UniformMatrix4fv(self.model_location, 1, GL_FALSE, model.as_ptr());
+            check_error("bind the viewmodel")?;
 
+            gl::ActiveTexture(gl::TEXTURE0);
             gl::BindTexture(gl::TEXTURE_2D, self.texture_id);
+            gl::Uniform1i(self.texture_location, 0);
+            check_error("bind the texture")?;
 
             gl::BindBuffer(gl::ARRAY_BUFFER, self.vertex_buffer_id);
             gl::VertexAttribPointer(self.position_location, 2, gl::FLOAT, GL_FALSE, 0, gl_voidptr!(QUAD_VERTICES));
             gl::DrawArrays(gl::TRIANGLES, 0, 6);
+            check_error("draw things")?;
+            Ok(())
         }
     }
 }
