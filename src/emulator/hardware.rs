b335edd7099c8c81a9bf7bef151a073d5934094b
Simon Gellis <simongellis@gmail.com>
2021-04-15 23:23:58 -0400

    Make timing of timer-with-reload-value-1 match hardware

--- src/emulator/hardware.rs
+++ src/emulator/hardware.rs
@@ -186,7 +186,7 @@ impl Hardware {
         let tcr = memory.read_byte(TCR);
         let enabled = (tcr & T_ENABLED) != 0;
         if enabled && self.reload_value != 0 {
-            let interval = if (tcr & T_INTERVAL) != 0 { 400 } else { 2000 };
+            let interval = if self.reload_value == 1 { 210 } else if (tcr & T_INTERVAL) != 0 { 400 } else { 2000 };
             self.next_tick = self.cycle + interval;
         } else {
             self.next_tick = u64::MAX;
