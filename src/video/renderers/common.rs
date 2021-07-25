518340f4a6aada10ec8cfe734d1a821192eb15da
Simon Gellis <simongellis@gmail.com>
2021-07-24 22:46:17 -0400

    Make disconnected renderers do nothing, instead of error

--- src/video/renderers/common.rs
+++ src/video/renderers/common.rs
@@ -11,36 +11,48 @@ pub trait RenderLogic {
 
 pub struct Renderer<TLogic: RenderLogic> {
     frame_channel: FrameChannel,
+    disconnected: bool,
     pub logic: TLogic,
 }
 impl<TLogic: RenderLogic> Renderer<TLogic> {
     pub fn new(frame_channel: FrameChannel, logic: TLogic) -> Self {
         Self {
             frame_channel,
+            disconnected: false,
             logic,
         }
     }
 
     pub fn on_surface_created(&mut self) -> Result<()> {
+        if self.disconnected {
+            return Ok(());
+        }
         self.logic.init()
     }
 
     pub fn on_surface_changed(&mut self, width: i32, height: i32) -> Result<()> {
+        if self.disconnected {
+            return Ok(());
+        }
         self.logic.resize((width, height))
     }
 
     pub fn on_draw_frame(&mut self) -> Result<()> {
+        if self.disconnected {
+            return Ok(());
+        }
         match self.frame_channel.try_recv() {
             Ok(frame) => {
                 let eye = frame.eye;
                 let buffer = frame.buffer.lock().expect("Buffer lock was poisoned!");
                 self.logic.update(eye, &buffer)?;
+                self.logic.draw()
             }
-            Err(TryRecvError::Empty) => (),
+            Err(TryRecvError::Empty) => self.logic.draw(),
             Err(TryRecvError::Disconnected) => {
-                return Err(anyhow::anyhow!("Emulator has shut down"))
+                self.disconnected = true;
+                Ok(())
             }
-        };
-        self.logic.draw()
+        }
     }
 }
