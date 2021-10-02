89a25579ec03e40daca3a37713d579786e87c3b5
jake <mail@jakedowns.com>
2021-10-02 00:25:52 -0700

    toggle immersive view options

--- app/src/main/java/com/simongellis/vvb/game/GameActivity.kt
+++ app/src/main/java/com/simongellis/vvb/game/GameActivity.kt
@@ -1,9 +1,12 @@
 package com.simongellis.vvb.game
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.KeyEvent
 import android.view.MotionEvent
+import android.view.View
 import androidx.activity.viewModels
 import com.simongellis.vvb.emulator.*
 
@@ -15,6 +18,8 @@ class GameActivity : AppCompatActivity() {
     private lateinit var _controller: Controller
     private lateinit var _inputBindingMapper: InputBindingMapper
     private lateinit var _preferences: GamePreferences
+
+    private lateinit var mDecorView: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +39,24 @@ class GameActivity : AppCompatActivity() {
         _preferences = preferences
 
         viewModel.loadPreviewImage()
+
+        if(savedInstanceState == null){
+            // Get a handle to the Window containing the UI.
+            mDecorView = window.decorView
+            // does this need re-bound onResume?
+            mDecorView.setOnSystemUiVisibilityChangeListener(View.OnSystemUiVisibilityChangeListener { visibility: Int ->
+                if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
+                    // Go back to immersive fullscreen mode in 3s
+                    val handler = Handler(mainLooper)
+                    handler.postDelayed(
+                        { this.toggleImmersiveView(true) },
+                        3000 /* 3s */
+                    )
+                }
+            })
+        }
+        // should this move into the view?
+        toggleImmersiveView(true)
     }
 
     override fun dispatchKeyEvent(event: KeyEvent): Boolean {
@@ -60,6 +83,8 @@ class GameActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        // should this move into the view?
+        toggleImmersiveView(true)
         _view.onResume()
         _audio.start()
         viewModel.resumeGame()
@@ -78,5 +103,26 @@ class GameActivity : AppCompatActivity() {
         _view.controller = null
         _controller.destroy()
         _audio.destroy()
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        toggleImmersiveView(hasFocus)
+    }
+
+    // fires onCreate, onResume
+    fun toggleImmersiveView(immersive: Boolean) {
+        // todo: make this a user-configurable opt-in "optimization" (trade-off)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            window.setSustainedPerformanceMode(immersive)
+        }
+        if(immersive){
+            mDecorView.systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE
+        }
     }
 }
