ce9bcddf678089cc4fe630ba5f292322e7ac97c7
jake <mail@jakedowns.com>
2022-06-25 00:10:07 -0700

    fix compiler warning

--- app/src/main/java/com/simongellis/vvb/game/GameActivity.kt
+++ app/src/main/java/com/simongellis/vvb/game/GameActivity.kt
@@ -47,19 +47,7 @@ class GameActivity : AppCompatActivity() {
         viewModel.loadPreviewImage()
 
         // Get a handle to the Window containing the UI.
-        mDecorView = window.decorView
-        // does this need re-bound onResume?
-        mDecorView.setOnSystemUiVisibilityChangeListener(View.OnSystemUiVisibilityChangeListener { visibility: Int ->
-            if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
-                // Go back to immersive fullscreen mode in 3s
-                val handler = Handler(mainLooper)
-                handler.postDelayed(
-                    { this.toggleImmersiveView(true) },
-                    3000 /* 3s */
-                )
-            }
-        })
-        // should this move into the view?
+        // mDecorView = window.decorView
         toggleImmersiveView(true)
     }
 
@@ -138,14 +126,21 @@ class GameActivity : AppCompatActivity() {
         }else{
             window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         }
-        if(immersive && mDecorView !== null){
-            mDecorView.systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_IMMERSIVE
+        if(immersive){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                window.attributes.layoutInDisplayCutoutMode =
+                    WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                @Suppress("DEPRECATION")
+                window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+            }
         }
     }
 }
