df38cdcd7af34a31763e8e5282bca8efcde8aff6
jake <mail@jakedowns.com>
2022-06-30 17:58:20 -0700

    move leia mode up

--- app/src/main/java/com/simongellis/vvb/game/VideoMode.kt
+++ app/src/main/java/com/simongellis/vvb/game/VideoMode.kt
@@ -8,10 +8,10 @@ enum class VideoMode(
     @StringRes val summary: Int,
     @StringRes val description: Int
 ) {
+    LEIA(true, R.string.video_mode_leia_summary, R.string.video_mode_leia_description),
     ANAGLYPH(true, R.string.video_mode_anaglyph_summary, R.string.video_mode_anaglyph_description),
     CARDBOARD(false, R.string.video_mode_cardboard_summary, R.string.video_mode_cardboard_description),
     MONO_LEFT(true, R.string.video_mode_mono_left_summary, R.string.video_mode_mono_description),
     MONO_RIGHT(true, R.string.video_mode_mono_right_summary, R.string.video_mode_mono_description),
-    STEREO(false, R.string.video_mode_stereo_summary, R.string.video_mode_stereo_description),
-    LEIA(true, R.string.video_mode_leia_summary, R.string.video_mode_leia_description);
+    STEREO(false, R.string.video_mode_stereo_summary, R.string.video_mode_stereo_description);
 }
