8b4ad2a13d80685e8da681442a2f0650cb2666d1
Simon Gellis <simongellis@gmail.com>
2024-01-18 23:24:17 -0500

    Update another dep

--- app/src/main/java/com/simongellis/vvb/menu/ControllerNameViewModel.kt
+++ app/src/main/java/com/simongellis/vvb/menu/ControllerNameViewModel.kt
@@ -5,5 +5,5 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class ControllerNameViewModel(savedStateHandle: SavedStateHandle): ViewModel() {
-    val name = MutableStateFlow(savedStateHandle.get("initialValue") ?: "")
+    val name = MutableStateFlow(savedStateHandle["initialValue"] ?: "")
 }
