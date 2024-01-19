976d8ef800a5e4ac04ac4f5fc86ce181a2fd7ab4
Simon Gellis <simongellis@gmail.com>
2024-01-18 23:18:51 -0500

    Silence warning in generated code

--- src/video/gl.rs
+++ src/video/gl.rs
@@ -1,4 +1,5 @@
 #![allow(clippy::all)]
+#![allow(unused_imports)]
 
 #[cfg(target_os = "android")]
 #[link(name = "GLESv2")]
@@ -6,5 +7,6 @@ extern "C" {}
 
 include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
 
+#[warn(unused_imports)]
 #[warn(clippy::all)]
 pub mod utils;
